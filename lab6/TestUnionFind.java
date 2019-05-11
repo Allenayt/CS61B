4daa2df42e4be3aee7d5839f1a51374a2867cfce
Yuting An <allenayt1997@gmail.com>
2019-05-11 17:28:10 +0800

    Lab6 in May 11st

---
+++ lab6/TestUnionFind.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestUnionFind {
+
+    @Test
+    public void testUnionFind() {
+        UnionFind lst = new UnionFind(5);
+        assertEquals(lst.find(4), 4);
+        lst.union(2, 4);
+        lst.union(1, 3);
+        assertEquals(lst.find(2), 4);
+        assertTrue(lst.connected(2, 4));
+        assertFalse(lst.connected(1, 0));
+        assertEquals(lst.sizeOf(1), 2);
+        /*********************/
+        lst.union(1, 0);
+        assertEquals(lst.sizeOf(1), 3);
+        assertFalse(lst.connected(2, 0));
+        /*********************/
+        lst.union(2, 0);
+        assertEquals(lst.sizeOf(4), 5);
+        assertTrue(lst.connected(4, 1));
+    }
+
+}
